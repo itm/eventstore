17e27fc34e1a238e239204cb3c17d2e3674e7f80
Tobias Mende <tobias.mende@tobsolution.de>
2014-02-13 13:51:26 +0100

    fixing syntactical errors

--- src/main/java/eventstore/ChronicleBasedEventStore.java
+++ src/main/java/eventstore/ChronicleBasedEventStore.java
@@ -70,7 +70,6 @@ public class ChronicleBasedEventStore<T> implements IEventStore<T> {
         storeEvent(object, c);
 
     }
-}
 
     @Override
     public void storeEvent(@Nonnull T object, Class<T> type) throws IOException {
@@ -93,7 +92,7 @@ public class ChronicleBasedEventStore<T> implements IEventStore<T> {
 
     @Override
     public Iterator<IEventContainer<T>> getEventsBetweenTimestamps(long fromTime, long toTime) throws IOException {
-        return new LimitedEventIterator<T>(fromTime, toTime);
+        return new LimitedEventIterator(fromTime, toTime);
     }
 
     @Override
@@ -116,117 +115,117 @@ public class ChronicleBasedEventStore<T> implements IEventStore<T> {
         }
     }
 
-private abstract class AbstractEventIterator<T> implements java.util.Iterator<IEventContainer<T>> {
+    private abstract class AbstractEventIterator implements java.util.Iterator<IEventContainer<T>> {
 
-    protected ExcerptTailer reader;
+        protected ExcerptTailer reader;
 
-    protected IEventContainer<T> next;
+        protected IEventContainer<T> next;
 
-    public AbstractEventIterator(long fromTime) throws IOException {
-        reader = chronicle.createTailer();
-        if (fromTime > 0) {
-            windToTimestamp(fromTime);
-        } else {
-            next = readNextEvent();
+        public AbstractEventIterator(long fromTime) throws IOException {
+            reader = chronicle.createTailer();
+            if (fromTime > 0) {
+                windToTimestamp(fromTime);
+            } else {
+                next = readNextEvent();
+            }
         }
+
+        private boolean windToTimestamp(long timestamp) {
+            long firstTimestamp;
+            while (reader.nextIndex()) {
+                firstTimestamp = reader.readLong();
+                if (firstTimestamp >= timestamp) {
+                    byte type = reader.readByte();
+                    byte[] event = new byte[(int) reader.remaining()];
+                    reader.readFully(event);
+                    Function<byte[], T> decoder = deserializers.get(type);
+                    T object = decoder.apply(event);
+
+                    next = new DefaultEventContainer<T>(object, timestamp);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        protected abstract IEventContainer<T> readNextEvent();
+
+
+        @Override
+        public boolean hasNext() {
+            if (next == null) {
+                next = readNextEvent();
+            }
+            return next != null;
+        }
+
+        @Override
+        public IEventContainer<T> next() {
+            if (next != null) {
+                IEventContainer<T> event = next;
+                next = readNextEvent();
+                return event;
+            }
+
+            throw new NoSuchElementException();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
     }
 
-    private boolean windToTimestamp(long timestamp) {
-        long firstTimestamp;
-        while (reader.nextIndex()) {
-            firstTimestamp = reader.readLong();
-            if (firstTimestamp >= timestamp) {
+    private class InfiniteEventIterator extends AbstractEventIterator {
+
+        public InfiniteEventIterator(long fromTime) throws IOException {
+            super(fromTime);
+        }
+
+        @Override
+        protected IEventContainer<T> readNextEvent() {
+            if (reader.nextIndex()) {
+                long timestamp = reader.readLong();
                 byte type = reader.readByte();
                 byte[] event = new byte[(int) reader.remaining()];
                 reader.readFully(event);
-                Function<byte[], T> decoder = deserializers.get(type);
-                T object = decoder.apply(event);
-
-                next = new DefaultEventContainer<T>(object, timestamp);
-                return true;
+                T object = deserializers.get(type).apply(event);
+                return new DefaultEventContainer<T>(object, timestamp);
             }
+            reader.finish();
+            return null;
         }
-        return false;
     }
 
-    protected abstract IEventContainer<T> readNextEvent();
+    /**
+     * The limited event iterator is returned for iterating through events between two different timestamps
+     */
+    private class LimitedEventIterator extends AbstractEventIterator {
 
+        private long toTime;
 
-    @Override
-    public boolean hasNext() {
-        if (next == null) {
-            next = readNextEvent();
-        }
-        return next != null;
-    }
-
-    @Override
-    public IEventContainer<T> next() {
-        if (next != null) {
-            IEventContainer<T> event = next;
-            next = readNextEvent();
-            return event;
+        public LimitedEventIterator(long fromTime, long toTime) throws IOException {
+            super(fromTime);
+            this.toTime = toTime;
         }
 
-        throw new NoSuchElementException();
-    }
-
-    @Override
-    public void remove() {
-        throw new UnsupportedOperationException();
-    }
-
-}
-
-private class InfiniteEventIterator<T> extends AbstractEventIterator<T> {
-
-    public InfiniteEventIterator(long fromTime) throws IOException {
-        super(fromTime);
-    }
-
-    @Override
-    protected IEventContainer<T> readNextEvent() {
-        if (reader.nextIndex()) {
-            long timestamp = reader.readLong();
-            byte type = reader.readByte();
-            byte[] event = new byte[(int) reader.remaining()];
-            reader.readFully(event);
-            T object = deserializers.get(type).apply(event);
-            return new DefaultEventContainer<T>(object, timestamp);
-        }
-        reader.finish();
-        return null;
-    }
-}
-
-/**
- * The limited event iterator is returned for iterating through events between two different timestamps
- */
-private class LimitedEventIterator<T> extends AbstractEventIterator<T> {
-
-    private long toTime;
-
-    public LimitedEventIterator(long fromTime, long toTime) throws IOException {
-        super(fromTime);
-        this.toTime = toTime;
-    }
-
-    @Override
-    protected IEventContainer<T> readNextEvent() {
-        if (reader.nextIndex()) {
-            long timestamp = reader.readLong();
-            if (timestamp > toTime) {
-                reader.finish();
-                return null;
+        @Override
+        protected IEventContainer<T> readNextEvent() {
+            if (reader.nextIndex()) {
+                long timestamp = reader.readLong();
+                if (timestamp > toTime) {
+                    reader.finish();
+                    return null;
+                }
+                byte type = reader.readByte();
+                byte[] event = new byte[(int) reader.remaining()];
+                reader.readFully(event);
+                T object = deserializers.get(type).apply(event);
+                return new DefaultEventContainer<T>(object, timestamp);
             }
-            byte type = reader.readByte();
-            byte[] event = new byte[(int) reader.remaining()];
-            reader.readFully(event);
-            T object = deserializers.get(type).apply(event);
-            return new DefaultEventContainer<T>(object, timestamp);
+            reader.finish();
+            return null;
         }
-        reader.finish();
-        return null;
     }
-}
 }
